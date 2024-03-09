57172f56578da703bdcfe989bad2e90c208b5dfa
Syed Raza Ali <razaali26296@gmail.com>
2024-03-09 22:17:09 +0530

    page deployment commit

--- src/components/meme.js
+++ src/components/meme.js
@@ -1,8 +1,15 @@
 import React from "react";
+import memesData from "../memesData";
 export default function header() {
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log(url);
+  }
   return (
-    <main className="p-9">
-      <form className="grid grid-cols-2 grid-rows-1 gap-4">
+    <main className="p-9 ">
+      <div className="grid grid-cols-2 grid-rows-1 gap-4">
         <input
           className="indent-2 font-sans rounded-md border border-solid border-[#D5D4D8]"
           placeholder="Top Text"
@@ -14,12 +21,13 @@ export default function header() {
           type="text"
         />
         <button
-          className="col-span-2 font-sans text-white rounded-md bg-gradient-to-r from-indigo-600 to-pink-600 "
+          className="col-span-2 font-sans text-white border-none rounded-md cursor-pointer bg-gradient-to-r from-purple-600 to-purple-400 "
           type="button"
+          onClick={getMemeImage}
         >
           Get a New Meme Image
         </button>
-      </form>
+      </div>
     </main>
   );
 }
